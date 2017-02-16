d5543e71f332733aeb173311a89c637458de36b2
SB00478824 <SB00478824@INBASDPC12294>
2017-02-16 11:57:38 +0530

    Add project files.

---
+++ 9thfeb_readkey/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9thfeb_readkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iNum1=0;
+            Console.Write("Enter one number: ");
+            iNum1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The entered key is: {0}",iNum1);
+            StringBuilder sc = new StringBuilder();
+            sc.Append("Hello Sarang! \n");
+            sc.AppendLine("The Format is wrong");
+            Console.WriteLine(sc);
+        }
+    }
+}
